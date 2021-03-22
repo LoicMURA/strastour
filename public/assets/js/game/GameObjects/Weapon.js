7dd86e49c7a1b88cd02b9731b802fff50fa027ee
Christian Tomasino <christian.tomasino.dev@gmail.com>
2021-03-22 17:11:54 +0100

    Debug

---
+++ public/assets/js/game/GameObjects/Weapon.js
@@ -0,0 +1,9 @@
+export default class Weapon{
+    constructor() {
+        this.type = ''; // aoe - cac
+        this.bonusDamage = 0;
+        this.fireRate = 0;
+        this.attack = null; // new Attack
+        this.quality = 100;
+    }
+}
