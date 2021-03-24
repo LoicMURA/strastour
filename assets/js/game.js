ea2673ea1c21e86cbe7e67502edce7c2c9f76bf1
TOMASINO CHRISTIAN <christian.tomasino@etu.unistra.fr>
2021-03-24 15:53:54 +0000

    Feature/board

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,4 @@
 import '../scss/app.scss';
 import Level from "./game/Map/Level.js";
-import Item from "./game/GameObjects/Item";
-import Player from "./game/Characters/Player";
-
-const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d');
 
 new Level(ID_LEVEL ?? 0);
-let item = {
-    id : "3",
-    name : "salut"
-}
-new Item(item);
-// let course;
-// let place;
-// new Level(course.id ?? 0, place.id ?? 0);
