dd94a710500f9144f6b0928a25c8fc5c25eb6532
MURA LOIC <loic.mura@etu.unistra.fr>
2021-03-30 13:29:38 +0000

    Item quantity in the HUD

--- assets/js/game/HUD.js
+++ assets/js/game/HUD.js
@@ -22,7 +22,7 @@ export default class HUD {
 
         this.equipement.addEventListener('click', (e) => {
             let parent = e.target.parentNode
-            if (parent.classList.contains('items') && parent.children.length === 4) {
+            if (parent.classList.contains('items') && parent.children.length === 4 && !parent.classList.contains('items__empty')) {
                 this.setActiveItem(parent, player)
             }
         })
@@ -32,7 +32,7 @@ export default class HUD {
                 equipementId = parseInt(key.code.replace(/Numpad|Digit/, ''))
                 if (equipementId > 0 && equipementId <= 6) {
                     let itemBox = this.equipement.children[equipementId - 1];
-                    if (itemBox.children.length === 4) {
+                    if (itemBox.children.length === 4 && !itemBox.classList.contains('items__empty')) {
                         this.setActiveItem(this.equipement.children[equipementId - 1], player)
                     }
                 }
@@ -46,6 +46,13 @@ export default class HUD {
         Array.from(this.equipement.children).forEach(equipement => {
             if (index < player.equipement.length) {
                 let item = player.equipement[index];
+                let itemQuantity = 0;
+                for (let itemInInventory of player.inventory) {
+                    if (itemInInventory.item.id === item.id) {
+                        itemQuantity = itemInInventory.quantity
+                    }
+                }
+                this.updateItemQuantity(index, itemQuantity, player)
                 equipement.setAttribute('data-item', item.id);
                 let itemImage = document.createElement("img");
                 itemImage.alt =`Image de l'item ${item.name}`;
@@ -100,5 +107,22 @@ export default class HUD {
                 item.classList.add('items__active')
             }
         }
+    }
+
+    updateItemQuantity(index, quantity, player) {
+        let itemBox = this.equipement.children[index];
+        if (quantity === 0) {
+            itemBox.classList.add('items__empty')
+            if (itemBox.classList.contains('items__active')) {
+                itemBox.classList.remove('items__active')
+                for (let item of this.equipement.children) {
+                    if (item.querySelector('.items__quantity').innerText != '0') {
+                        this.setActiveItem(item, player)
+                        break;
+                    }
+                }
+            }
+        }
+        if (quantity >= 0) itemBox.querySelector('.items__quantity').innerText = quantity
     }
 }
